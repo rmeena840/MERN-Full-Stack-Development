c17598bafa5204155b12e35a4df6a4dfac2b2cec
Ravinda <ravindra.iitism@gmail.com>
2020-03-25 12:09:17 +0530

    Shopping List componenet added

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Login from './components/Login';
 import AppNavBar from './components/AppNavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ShoppingList from './components/ShoppingList';
 
 function App() {
   return (
     <div className="App">
       {/* <Login /> */}
       <AppNavBar />
+      <ShoppingList />
     </div>
   );
 }
